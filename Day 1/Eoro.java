d363a2d7e8fe9c5543ceec6265d131c4b2845684
Mohammed Sulthan <msulthan139@gmail.com>
2025-12-11 10:24:01 +0530

    Added Day 1 and .gitignore file

---
+++ Day 1/Eoro.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Eoro {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2==0){
+            System.out.println(a+" Even Number");
+        }
+        else{
+            System.out.println(a+" Odd Number");
+        }
+    }
+}
---
+++ Day 1/Got.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class Got {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if (a>b) {
+            System.out.println(a+" is greater than B");
+        }
+        else{
+            System.out.println(b+" is greater than A");
+        }
+    }
+    
+}
